4eab0032b5aebeadbd51de8e23f4d25720cdbecd
Marcus Hellberg <marcus@vaadin.com>
2020-01-16 17:07:45 -0800

    initial

---
+++ src/main/java/com/vaadin/tutorial/crm/GreetService.java
@@ -0,0 +1,18 @@
+package com.vaadin.tutorial.crm;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetService implements Serializable {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+
+}
---
+++ src/main/java/com/vaadin/tutorial/crm/MainView.java
@@ -0,0 +1,67 @@
+package com.vaadin.tutorial.crm;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.",
+        enableInstallPrompt = true)
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    /**
+     * Construct a new Vaadin view.
+     * <p>
+     * Build the initial UI state for the user accessing the application.
+     *
+     * @param service The message service. Automatically injected Spring managed bean.
+     */
+    public MainView(@Autowired GreetService service) {
+
+        // USe TextField for standard text input
+        TextField textField = new TextField("Your name");
+
+        // Button click listeners can be defined as lambda expressions
+        Button button = new Button("Say hello",
+                e -> Notification.show(service.greet(textField.getValue())));
+
+        // Theme variants give you predefined extra styles for components.
+        // Example: Primary button is more prominent look.
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        // You can specify keyboard shortcuts for buttons.
+        // Example: Pressing enter in this view clicks the Button.
+        button.addClickShortcut(Key.ENTER);
+
+        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+        addClassName("centered-content");
+
+        add(textField, button);
+    }
+
+}
