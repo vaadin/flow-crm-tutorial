a61058f8c2d9babe9bf0d966e4beefac220dad3e
Marcus Hellberg <marcus@vaadin.com>
2020-01-17 13:45:13 -0800

    Chapter 2

---
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -0,0 +1,13 @@
+package com.vaadin.tutorial.crm.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+
+}
