16d32958b830a9004187ef0a470362c82fedba7e
Marcus Hellberg <marcus@vaadin.com>
2020-01-22 14:36:21 -0800

    Listing contacts in a grid

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,13 +1,43 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Company;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    private ContactService contactService;
 
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+
+        add(grid);
+        updateList();
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+        grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+           Company company = contact.getCompany();
+           return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
     }
 
 }
