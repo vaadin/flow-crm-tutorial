b388b1ed1a7943135a34265240e9f9d7a50164ad
Marcus Hellberg <marcus@vaadin.com>
2020-12-15 10:09:05 -0800

    use new api

--- src/main/java/com/vaadin/tutorial/crm/security/SecurityUtils.java
+++ src/main/java/com/vaadin/tutorial/crm/security/SecurityUtils.java
@@ -1,13 +1,15 @@
 package com.vaadin.tutorial.crm.security;
 
-import com.vaadin.flow.server.ServletHelper.RequestType;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.vaadin.flow.server.HandlerHelper.RequestType;
 import com.vaadin.flow.shared.ApplicationConstants;
+
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.stream.Stream;
 
 /**
  * SecurityUtils takes care of all such static operations that have to do with
@@ -15,32 +17,32 @@ import java.util.stream.Stream;
  */
 public final class SecurityUtils {
 
-    private SecurityUtils() {
-        // Util methods only
-    }
+  private SecurityUtils() {
+    // Util methods only
+  }
 
-    /**
-     * Tests if the request is an internal framework request. The test consists of
-     * checking if the request parameter is present and if its value is consistent
-     * with any of the request types know.
-     *
-     * @param request {@link HttpServletRequest}
-     * @return true if is an internal framework request. False otherwise.
-     */
-    static boolean isFrameworkInternalRequest(HttpServletRequest request) {
-        final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
-        return parameterValue != null
-            && Stream.of(RequestType.values()).anyMatch(r -> r.getIdentifier().equals(parameterValue));
-    }
+  /**
+   * Tests if the request is an internal framework request. The test consists of
+   * checking if the request parameter is present and if its value is consistent
+   * with any of the request types know.
+   *
+   * @param request {@link HttpServletRequest}
+   * @return true if is an internal framework request. False otherwise.
+   */
+  static boolean isFrameworkInternalRequest(HttpServletRequest request) {
+    final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
+    return parameterValue != null
+        && Stream.of(RequestType.values()).anyMatch(r -> r.getIdentifier().equals(parameterValue));
+  }
 
-    /**
-     * Tests if some user is authenticated. As Spring Security always will create an {@link AnonymousAuthenticationToken}
-     * we have to ignore those tokens explicitly.
-     */
-    static boolean isUserLoggedIn() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication != null
-            && !(authentication instanceof AnonymousAuthenticationToken)
-            && authentication.isAuthenticated();
-    }
+  /**
+   * Tests if some user is authenticated. As Spring Security always will create an
+   * {@link AnonymousAuthenticationToken} we have to ignore those tokens
+   * explicitly.
+   */
+  static boolean isUserLoggedIn() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    return authentication != null && !(authentication instanceof AnonymousAuthenticationToken)
+        && authentication.isAuthenticated();
+  }
 }
