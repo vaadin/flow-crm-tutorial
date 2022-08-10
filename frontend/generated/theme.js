d07d820e0c5af2dca03575079a74f38959d30855
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-08-10 13:47:11 +0300

    Delete frontend/generated directory

--- frontend/generated/theme.js
+++
@@ -1,2 +0,0 @@
-import {applyTheme as _applyTheme} from './theme-flowcrmtutorial.generated.js';
-export const applyTheme = _applyTheme;
--- frontend/generated/vaadin.ts
+++
@@ -1,6 +0,0 @@
-import './vaadin-featureflags.ts';
-
-import './index';
-
-import { applyTheme } from './theme';
-applyTheme(document);
