0753f63c4e46878e1123024736913e69c5cb2780
Matti Tahvonen <matti@vaadin.com>
2022-10-06 15:36:30 +0300

    Use JUnit 5 for unit test side

--- src/test/java/com/example/application/views/list/ListViewTest.java
+++ src/test/java/com/example/application/views/list/ListViewTest.java
@@ -3,14 +3,14 @@ package com.example.application.views.list;
 import com.example.application.data.entity.Contact;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.data.provider.ListDataProvider;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class ListViewTest {
 
@@ -24,10 +24,10 @@ public class ListViewTest {
 
         ContactForm form = listView.form;
 
-        Assert.assertFalse(form.isVisible());
+        assertFalse(form.isVisible());
         grid.asSingleSelect().setValue(firstContact);
-        Assert.assertTrue(form.isVisible());
-        Assert.assertEquals(firstContact.getFirstName(), form.firstName.getValue());
+        assertTrue(form.isVisible());
+        assertEquals(firstContact.getFirstName(), form.firstName.getValue());
     }
     private Contact getFirstItem(Grid<Contact> grid) {
         return( (ListDataProvider<Contact>) grid.getDataProvider()).getItems().iterator().next();
