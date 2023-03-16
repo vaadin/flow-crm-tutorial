27ca404fc31b5af44a53368ed11f501db624cfd1
Matti Tahvonen <matti@vaadin.com>
2023-03-16 10:53:07 +0200

    fixed version of TestBench

--- src/test/java/com/example/application/it/LoginE2ETest.java
+++ src/test/java/com/example/application/it/LoginE2ETest.java
@@ -1,26 +1,17 @@
 package com.example.application.it;
 
 import com.example.application.it.elements.LoginViewElement;
-import com.vaadin.flow.component.applayout.testbench.AppLayoutElement;
-import com.vaadin.flow.component.login.testbench.LoginFormElement;
 import com.vaadin.testbench.BrowserTest;
 import com.vaadin.testbench.BrowserTestBase;
-import com.vaadin.testbench.annotations.RunLocally;
-import com.vaadin.testbench.parallel.Browser;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunLocally(Browser.FIREFOX)
+//@RunLocally(Browser.FIREFOX)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LoginE2ETest extends BrowserTestBase {
 
