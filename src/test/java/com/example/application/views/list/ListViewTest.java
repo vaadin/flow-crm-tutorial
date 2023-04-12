77e1692c664c0f64d0a08962be522c2246ee1f17
Matti Tahvonen <matti@vaadin.com>
2023-04-12 10:54:55 +0300

    Removed workaround for a regression (and updated versions)

--- src/test/java/com/example/application/views/list/ListViewTest.java
+++ src/test/java/com/example/application/views/list/ListViewTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class ListViewTest {
 
     static {
