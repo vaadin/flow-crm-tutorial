5e6458c678ac349dfa90e948ba268da05ea73adc
Mikaelsu <mikaelsu@vaadin.com>
2023-08-10 15:27:12 +0300

    Applying Knoobie

--- src/main/java/com/example/application/security/SecurityConfig.java
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.application.views.LoginView;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -19,7 +20,8 @@ public class SecurityConfig extends VaadinWebSecurity { // <2>
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth ->
-                auth.requestMatchers(new AntPathRequestMatcher("/images/*.png")).permitAll());  // <3>
+                auth.requestMatchers(
+                    AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/images/*.png")).permitAll());  // <3>
         super.configure(http);
         setLoginView(http, LoginView.class); // <4>
     }
